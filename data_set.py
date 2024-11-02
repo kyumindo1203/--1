6c09b565ead0cc9c2014e4af20c156bc082d9be0
kyumindo1203 <kyumindo1203@naver.com>
2024-11-02 23:18:10 +0900

    data_set에 전처리 포멧 커스텀 모델 안에 넣음

--- data_set.py
+++ data_set.py
@@ -5,9 +5,17 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 #커스텀 이미지 전처리 시키기
 class MyDataSet(Dataset):
-    def __init__(self, path, transform = None):
+    def __init__(self, path):
         self.path = path #파일 경로
-        self.transform = transform #전처리 유무
+        transform = transforms.Compose(
+            [
+                transforms.Resize((128,128)), #이미지 해상도 설정
+                transforms.RandomHorizontalFlip(p=0.5), #50퍼 확률로 좌우 반전 => 128해상도에서 학습 안되면 삭제할 예정
+                # transforms.RandomRotation(degrees=30, fill=(0,0,0)), #최대 30도까지 랜덤 회전 => 128해상도에서 학습 안되면 삭제할 예정 , 빈공간은 검은색으로 채우기 => 텐서 오류 발생
+                transforms.ToTensor()
+            ]
+        )
+        self.transform = transform
 
         self.image_paths = [] #이미지 경로들
         self.imgTypes = [] #이미지 종류
@@ -34,32 +42,37 @@ class MyDataSet(Dataset):
         
         if self.transform:
             image = self.transform(image) #텐서로 전처리
-        return image, imgType, img_path
+        return image, imgType#, img_path 
     
 
 if __name__ == '__main__':
     path = os.path.dirname(os.path.abspath(os.path.dirname(__file__))) #상위폴더
 
     #전처리 설정
-    transform = transforms.Compose(
-        [
-            transforms.Resize((128,128)), #이미지 해상도 설정
-            transforms.RandomHorizontalFlip(p=0.5), #50퍼 확률로 좌우 반전 => 128해상도에서 학습 안되면 삭제할 예정
-            # transforms.RandomRotation(degrees=30, fill=(0,0,0)), #최대 30도까지 랜덤 회전 => 128해상도에서 학습 안되면 삭제할 예정 , 빈공간은 검은색으로 채우기 => 텐서 오류 발생
-            transforms.ToTensor()
-        ]
-    )
+    # transform = transforms.Compose(
+    #     [
+    #         transforms.Resize((128,128)), #이미지 해상도 설정
+    #         transforms.RandomHorizontalFlip(p=0.5), #50퍼 확률로 좌우 반전 => 128해상도에서 학습 안되면 삭제할 예정
+    #         # transforms.RandomRotation(degrees=30, fill=(0,0,0)), #최대 30도까지 랜덤 회전 => 128해상도에서 학습 안되면 삭제할 예정 , 빈공간은 검은색으로 채우기 => 텐서 오류 발생
+    #         transforms.ToTensor()
+    #     ]
+    # )
 
     
     # d = open("Tensor_info.txt","w",encoding="utf-8")
-    a = MyDataSet(f"{path}\\DataSets\\images",transform=transform)
+    a = MyDataSet(f"{path}\\DataSets\\images")
     # for i in range(a.__len__()):
     #     Img, Type, img_path= a.__getitem__(i)
     #     d.write(f"(img:{Img}, type:{Type}, path:{img_path})\n")
     DataA = DataLoader(a, batch_size=32, shuffle=True) # 배치 설정
-    # for i, j, k in DataA:
+    for i, j in DataA:
     #     # d.write(f"(img:{Img}, type:{Type}, path:{img_path})\n")
     #     # d.write(f"i:{i}, j:{j}, k:{k}\n\n")
-    #     print(f"i : {i.shape}, j : {j.shape}")
+        print(f"i : {i.shape}, j : {j.shape}")
 
     # d.close()
+
+else:
+    path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    a = MyDataSet(f"{path}\\DataSets\\images")
+    DataA = DataLoader(a, batch_size=32, shuffle=True)
